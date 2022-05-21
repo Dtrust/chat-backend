fc59afe41842a775c6dff9a313df14318025fdb1
Dennis <1044442@gmail.com>
2022-05-21 22:06:21 +0300

    cors debug

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -24,7 +24,7 @@ const createRoutes = (app: express.Express, io: any) => {
     //     // credentials: true
     // }));
     app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Origin", "https://skymessenger.herokuapp.com");
         res.header(
             "Access-Control-Allow-Headers",
             "Origin, X-Requested-With, Content-Type, Accept"
